77de559d6bf8e73ae117a3f870853006faa67c14
midou-bit <midou@DESKTOP-GEB5SA4>
2025-05-14 16:01:57 +0200

    activite11.3

--- front-auth/src/components/PrivateRoute.jsx
+++ front-auth/src/components/PrivateRoute.jsx
@@ -7,9 +7,11 @@ const PrivateRoute = () => {
   useEffect(() => {
     const auth = JSON.parse(localStorage.getItem("auth"));
     const isValid = auth && new Date(auth.expiresAt) > new Date();
+
     if (!isValid) {
       localStorage.removeItem("auth");
       navigate("/connexion");
+      return;
     }
   }, [navigate]);
 
